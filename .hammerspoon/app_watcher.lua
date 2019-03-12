44d16eee743647425a3cc8348a6ff34c438b97c7
Geoffrey Bergeret <geoffrey@gbergeret.com>
2019-03-12 14:32:32 +0000

    Update Hammerspoon to always start terminal on tallest screen

--- .hammerspoon/app_watcher.lua
+++ .hammerspoon/app_watcher.lua
@@ -5,7 +5,19 @@
 appWatcher = hs.application.watcher.new(function(appName, event, app)
   -- Always launch Terminal in full screen.
   if event == hs.application.watcher.launched and appName == "Terminal" then
-    app:mainWindow():setFullScreen(true)
+
+    -- Find tallest screen
+    local initialScreen = hs.screen.primaryScreen()
+    local screen = initialScreen
+    local i = initialScreen
+    repeat
+      if i:currentMode()["h"] > screen:currentMode()["h"] then
+        screen = i
+      end
+      i = i:next()
+    until i:id() == initialScreen:id()
+
+    app:mainWindow():moveToScreen(screen):setFullScreen(true)
   end
 end)
 appWatcher:start()
