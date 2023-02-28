d889b3107c1008e00790f9bed911f3385e5fd5fc
Geoffrey Bergeret <geoffrey@gbergeret.com>
2023-02-28 11:03:54 +0000

    feat: Update App shortcuts

--- .hammerspoon/app_shortcuts.lua
+++ .hammerspoon/app_shortcuts.lua
@@ -1,40 +1,29 @@
 -----------------------------------------------
 -- App Shortcuts with Cmd
 -----------------------------------------------
--- Alt + 1 -- HEY / Asana
--- Alt + 2 -- Slack
--- Alt + 3 -- Google Chrome
--- Alt + 4 -- Terminal
--- Alt + 7 -- Music
--- Alt + 8 -- WhatsApp
--- Alt + 9 -- Messages
--- Alt + 0 -- Safari
+-- Ctrl + Alt + 1 -- Akiflow
+-- Ctrl + Alt + 2 -- Slack
+-- Ctrl + Alt + 3 -- Google Chrome
+-- Ctrl + Alt + 4 -- Terminal
+-- Ctrl + Alt + 9 -- Notes
+-- Ctrl + Alt + 0 -- Safari
 -----------------------------------------------
 
 local hyper = {"ctrl", "cmd"}
 
 -----------------------------------------------
--- hyper 1 to launch of focus on Mail
+-- hyper 1 to launch of focus on Akiflow
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, "1", "Jump to HEY (or Asana)", function()
-  -- if hs.application.frontmostApplication():name() == "HEY" then
-    -- hs.application.launchOrFocus("Asana")
-  -- else
-    hs.application.launchOrFocus("HEY")
-  -- end
+hs.hotkey.bind(hyper, "1", "Jump to Akiflow", function()
+    hs.application.launchOrFocus("Akiflow")
 end)
-
 -----------------------------------------------
 -- hyper 2 to launch of focus on Slack
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, "2", "Jump to Slack (or Microsoft Teams)", function()
-  -- if hs.application.frontmostApplication():name() == "Slack" then
-    -- hs.application.launchOrFocus("Microsoft Teams")
-  -- else
+hs.hotkey.bind(hyper, "2", "Jump to Slack", function()
     hs.application.launchOrFocus("Slack")
-  -- end
 end)
 
 -----------------------------------------------
@@ -55,35 +44,11 @@ hs.hotkey.bind(hyper, "4", "Jump to Terminal", function()
 end)
 
 -----------------------------------------------
--- hyper 6 to launch of focus on Music
+-- hyper 9 to launch of focus on Notes
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, "6", "Jump to Music", function()
-  hs.application.launchOrFocus("Music")
-end)
-
------------------------------------------------
--- hyper 7 to launch of focus on Music
------------------------------------------------
-
-hs.hotkey.bind(hyper, "7", "Jump to Signal", function()
-  hs.application.launchOrFocus("Signal")
-end)
-
------------------------------------------------
--- hyper 8 to launch of focus on WhatsApp
------------------------------------------------
-
-hs.hotkey.bind(hyper, "8", "Jump to WhatsApp", function()
-  hs.application.launchOrFocus("WhatsApp")
-end)
-
------------------------------------------------
--- hyper 9 to launch of focus on Messages
------------------------------------------------
-
-hs.hotkey.bind(hyper, "9", "Jump to Messages", function()
-  hs.application.launchOrFocus("Messages")
+hs.hotkey.bind(hyper, "9", "Jump to Notes", function()
+  hs.application.launchOrFocus("Notes")
 end)
 
 -----------------------------------------------
@@ -92,12 +57,4 @@ end)
 
 hs.hotkey.bind(hyper, "0", "Jump to Safari", function()
   hs.application.launchOrFocus("Safari")
-end)
-
------------------------------------------------
--- hyper b to launch of focus on Boop
------------------------------------------------
-
-hs.hotkey.bind(hyper, "b", "Boop", function()
-  hs.application.launchOrFocus("Boop")
 end)
--- .hammerspoon/fn_shortcuts.lua
+++ .hammerspoon/fn_shortcuts.lua
@@ -1,15 +1,50 @@
 -----------------------------------------------
 -- Function Shortcuts with Fn
 -----------------------------------------------
--- F3 -- Window Hint
+-- F1  -- Mail
+-- F2  -- Messages
+-- F3  -- WhatsApp
+-- F4  -- Messages
 -- F11 -- Show Desktop (default from OS X)
--- F12 -- Lock Screen
+-- F12 -- Finder
 -----------------------------------------------
 
 -----------------------------------------------
--- F3 to show window hints
+-- F1 to launch or focus on Mail
 -----------------------------------------------
 
-hs.hotkey.bind({}, "f3", "Show window Hints", function()
-  hs.hints.windowHints()
+hs.hotkey.bind({}, "f1", "Jump to Mail", function()
+  hs.application.launchOrFocus("Mail")
+end)
+
+-----------------------------------------------
+-- F2 to launch or focus on Messages
+-----------------------------------------------
+
+hs.hotkey.bind({}, "f2", "Jump to Messages", function()
+  hs.application.launchOrFocus("Messages")
+end)
+
+-----------------------------------------------
+-- F3 to launch or focus on WhatsApp
+-----------------------------------------------
+
+hs.hotkey.bind({}, "f3", "Jump to WhatsApp", function()
+  hs.application.launchOrFocus("WhatsApp")
+end)
+
+-----------------------------------------------
+-- F4 to launch or focus on Signal
+-----------------------------------------------
+
+hs.hotkey.bind({}, "f4", "Jump to Signal", function()
+  hs.application.launchOrFocus("Signal")
+end)
+
+-----------------------------------------------
+-- F12 to focus on Finder
+-----------------------------------------------
+
+hs.hotkey.bind({}, "f12", "Jump to Finder", function()
+  hs.application.launchOrFocus("Finder")
 end)
